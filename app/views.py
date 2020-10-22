d7dd1f0549b42fac6fcf7eec86be873436d51bf5
Cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2020-10-22 08:20:06 +0700

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,76 @@
+import datetime
+import json
+
+import requests
+from flask import render_template, redirect, request
+
+from app import app
+
+# The node with which our application interacts, there can be multiple
+# such nodes as well.
+CONNECTED_NODE_ADDRESS = "http://127.0.0.1:8000"
+
+posts = []
+
+
+def fetch_posts():
+    """
+    Function to fetch the chain from a blockchain node, parse the
+    data and store it locally.
+    """
+    get_chain_address = "{}/chain".format(CONNECTED_NODE_ADDRESS)
+    response = requests.get(get_chain_address)
+    if response.status_code == 200:
+        content = []
+        chain = json.loads(response.content)
+        for block in chain["chain"]:
+            for tx in block["transactions"]:
+                tx["index"] = block["index"]
+                tx["hash"] = block["previous_hash"]
+                content.append(tx)
+
+        global posts
+        posts = sorted(content, key=lambda k: k['timestamp'],
+                       reverse=True)
+
+
+@app.route('/')
+def index():
+    fetch_posts()
+    return render_template('index.html',
+                           title='YourNet: Public key sharing ',
+                           posts=posts,
+                           node_address=CONNECTED_NODE_ADDRESS,
+                           readable_time=timestamp_to_string)
+
+
+@app.route('/submit', methods=['POST'])
+def submit_textarea():
+    """
+    Endpoint to create a new transaction via our application.
+    """
+    pubkey = request.form["key"]
+    author = request.form["author"]
+    author_mail = request.form["author_mail"]
+    for ob in posts:
+        if author_mail == ob["author_mail"]:
+            return redirect('/')
+    post_object = {
+        'author': author,
+        'author_mail':author_mail,
+        'key': pubkey,
+
+    }
+
+    # Submit a transaction
+    new_tx_address = "{}/new_transaction".format(CONNECTED_NODE_ADDRESS)
+
+    requests.post(new_tx_address,
+                  json=post_object,
+                  headers={'Content-type': 'application/json'})
+
+    return redirect('/')
+
+
+def timestamp_to_string(epoch_time):
+    return datetime.datetime.fromtimestamp(epoch_time).strftime('%H:%M')
